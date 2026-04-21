52b0c6c2e1082f55828a309168ce7a8f44b840df
Ferdinando Villa <ferdinando.villa@gmail.com>
2026-04-21 16:57:54 +0200

    Remove unused dependencies and clean up `pom.xml` and `module-info.java`

---
+++ src/main/java/org/integratedmodelling/klab/ide/components/generic/TreeSearchField.java
@@ -0,0 +1,15 @@
+package org.integratedmodelling.klab.ide.components.generic;
+
+import javafx.scene.Node;
+import javafx.scene.control.TreeView;
+
+import java.util.Comparator;
+import java.util.function.Function;
+
+public class TreeSearchField<T> {
+
+    public TreeSearchField(TreeView<T> treeView, Function<String, T> itemMatcher) {
+
+    }
+
+}
